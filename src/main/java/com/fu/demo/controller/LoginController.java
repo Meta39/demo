177136f28e713d225e83213c3265ac047031a0bd
Meta39 <53995537@qq.com>
2022-05-21 11:07:41 +0800

    增加登陆时校验验证码的功能

--- src/main/java/com/fu/demo/controller/LoginController.java
+++ src/main/java/com/fu/demo/controller/LoginController.java
@@ -35,6 +35,13 @@ public class LoginController {
         return "hello";
     }
 
+    @GetMapping("getVCode")
+    public String getVCode(){
+        String vCode = String.format("%04d",new Random().nextInt(999999));//随机验证码
+        redisTemplate.opsForValue().set(vCode,null,120,TimeUnit.SECONDS);
+        return vCode;
+    }
+
     /**
      * 用户不在用户输入框的时候校验用户ID是否存在
      *
@@ -76,13 +83,17 @@ public class LoginController {
      * @return
      */
     @PostMapping("login")
-    public TokenInfo login(@RequestParam Long userId, @RequestParam String password){
+    public TokenInfo login(@RequestParam Long userId, @RequestParam String password,@RequestParam String vCode){
         User user = loginMapper.checkUserId(userId);
         if (user == null) {
             throw new Err("用户不存在");
         } else if (!RSAUtil.decrypt(password.replaceAll(" ", "+"), user.getPrivateKey()).equals(user.getPassword())) {
             throw new Err("密码错误");
-        } else {
+        }else if (!redisTemplate.hasKey(vCode)){
+            throw new Err("验证码错误或已失效！");
+        }else {
+            //删除验证成功的验证码
+            redisTemplate.delete(vCode);
             //把当前登录用户的信息存储到redis并返回给前端
             String token = UUID.randomUUID().toString();
             List<Role> roles = loginMapper.selectUserRoles(user.getId());//角色组
