7d4d3a1f67cc92dd304d70ec473abce91bd6df78
Meta39 <53995537@qq.com>
2022-06-08 14:22:14 +0800

    高可用mysql

--- src/main/java/com/fu/demo/druid/DataSourceAspect.java
+++ src/main/java/com/fu/demo/druid/DataSourceAspect.java
@@ -3,6 +3,7 @@ package com.fu.demo.druid;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.mybatis.spring.MyBatisSystemException;
 import org.springframework.core.annotation.Order;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
@@ -16,11 +17,26 @@ import java.util.List;
 @Component
 public class DataSourceAspect {
     @Resource
-    private RedisTemplate<String,Integer> redisTemplate;
+    private RedisTemplate<String, Integer> redisTemplate;
 
     @Around("execution(public * com.fu.demo.mapper.*.*(..))")
     public Object around(ProceedingJoinPoint point) throws Throwable {
-        //轮询数据库
+        try {
+            robin();//轮询数据库
+            return point.proceed();
+        } catch (MyBatisSystemException e) {
+            robin();//连接不上就再轮询一次，获取另外一个mysql数据库连接
+            return point.proceed();
+        } finally {
+            // 销毁数据源 在执行方法之后
+            DynamicDataSourceContextHolder.clearDateSourceType();
+        }
+    }
+
+    /**
+     * 轮询mysql数据库
+     */
+    public void robin(){
         String INDEX = "index";
         if (!redisTemplate.hasKey(INDEX)) redisTemplate.opsForValue().set(INDEX, 0);//如果key不存在就创建key,并设置下标初始值为0
         int index = redisTemplate.opsForValue().get(INDEX);//有下标则直接获取
@@ -30,11 +46,5 @@ public class DataSourceAspect {
         if (index >= list.size()) index = 0;//超过list集合的值就重新赋值（轮询）
         DynamicDataSourceContextHolder.setDateSourceType(list.get(index));
         redisTemplate.opsForValue().set(INDEX, ++index);//利用redis单线程的特性存放全局index下标
-        try {
-            return point.proceed();
-        } finally {
-            // 销毁数据源 在执行方法之后
-            DynamicDataSourceContextHolder.clearDateSourceType();
-        }
     }
 }
