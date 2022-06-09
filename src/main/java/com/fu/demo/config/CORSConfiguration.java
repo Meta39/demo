1d48cda315001d64d88f376c3287a2071d3edfb1
Meta39 <53995537@qq.com>
2022-06-10 01:05:04 +0800

    允许跨域访问

---
+++ src/main/java/com/fu/demo/config/CORSConfiguration.java
@@ -0,0 +1,21 @@
+package com.fu.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 允许跨越访问
+ */
+@Configuration
+public class CORSConfiguration implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
+}
