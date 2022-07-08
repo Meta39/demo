35dd274563bd293849084cbe9cbfb347f920d97d
Meta39 <53995537@qq.com>
2022-07-08 14:26:37 +0800

    fastdfs代码优化

--- src/main/java/com/fu/demo/fastdfs/FastDFSUtils.java
+++ src/main/java/com/fu/demo/fastdfs/FastDFSUtils.java
@@ -1,6 +1,5 @@
 package com.fu.demo.fastdfs;
 
-import com.fu.demo.entity.Err;
 import com.github.tobato.fastdfs.domain.fdfs.StorePath;
 import com.github.tobato.fastdfs.service.FastFileStorageClient;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +9,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import java.io.IOException;
 
 /**
  * FastDFS文件管理工具类
@@ -21,28 +21,16 @@ public class FastDFSUtils {
     @Resource
     private FastFileStorageClient fastFileStorageClient;
 
-    public String uploadFile(MultipartFile file){
-        String filePath;
-        try {
-            filePath = fastFileStorageClient.uploadFile(file.getInputStream(), file.getSize(), FilenameUtils.getExtension(file.getOriginalFilename()), null).getFullPath();
-        }catch (Exception e){
-            log.error("上传失败：",e);
-            throw new Err("上传失败："+e.getMessage());
-        }
-        return filePath;
+    public String uploadFile(MultipartFile file) throws IOException {
+        return fastFileStorageClient.uploadFile(file.getInputStream(), file.getSize(), FilenameUtils.getExtension(file.getOriginalFilename()), null).getFullPath();
     }
 
     public Boolean deleteFile(String filePath) {
         if (StringUtils.isEmpty(filePath)) {
             return false;
         }
-        try {
-            StorePath storePath = StorePath.parseFromUrl(filePath);
-            fastFileStorageClient.deleteFile(storePath.getGroup(), storePath.getPath());
-        } catch (Exception e) {
-            log.error("删除失败！", e);
-            throw new Err("删除失败："+e.getMessage());
-        }
+        StorePath storePath = StorePath.parseFromUrl(filePath);
+        fastFileStorageClient.deleteFile(storePath.getGroup(), storePath.getPath());
         return true;
     }
 }
